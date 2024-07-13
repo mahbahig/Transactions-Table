c1a9677134ee1013989afe101ce22772aa0a564a
mahbahig <mahbahig2@gmail.com>
2024-07-14 01:36:00 +0300

    Added Flags

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,23 @@
 "use strict"
 const dbPath = '../Database/transactionDB.json'
-jQuery(document).ready(function () { 
+let sortType = 'default';
+let sortAs = 'default';
+
+jQuery(document).ready(function () {
     const search = jQuery('#search');
+    const sortTypeSelect = jQuery('#sortType');
+    const sortAsSelect = jQuery('#sortAs');
+    const sort = jQuery('#sort');
+
+    sortTypeSelect.on('change', function () {
+        sortType = sortTypeSelect.val();
+    })
+    sortAsSelect.on('change', function () {
+        sortAs = sortAsSelect.val();
+    })
+    sort.on('click', function () {
+        displayData(data, sortType, sortAs);
+    })
     search.on('input', function () {
         console.log("Search Input");
     })
@@ -29,34 +45,23 @@ function displayData(data) {
     for (let index = 0; index < data.transactions.length; index++) {
         table.append(`
             <tr class="w-100 bg-lightest-blue text-white border-bottom border-dark-green">
-                <td class="text-center py-3">${index+1}</td>
+                <td class="text-center py-3">${index + 1}</td>
                 <td class="text-center py-3">${getCustomerName(data.transactions[index].customer_id, data)}</td>
                 <td class="text-center py-3">${data.transactions[index].customer_id}</td>
                 <td class="text-center py-3">${data.transactions[index].date}</td>
                 <td class="text-center py-3">${data.transactions[index].amount}</td>
                 <td class="text-center py-3">${data.transactions[index].id}</td>
             </tr>`);
-        
+
     }
 }
 
 function getCustomerName(customerId, data) {
     for (let index = 0; index < data.customers.length; index++) {
-        if (data.customers[index].id == customerId) { 
+        if (data.customers[index].id == customerId) {
             return data.customers[index].name;
         }
     }
 }
 
 readData();
-
-
-/*
-<tr class="w-100 bg-lightest-blue text-white border-bottom border-dark-green">
-    <td class="text-center py-3">getCustomerName(data.transactions[index].customer_id)</td>
-    <td class="text-center py-3">data.transactions[index].customer_id</td>
-    <td class="text-center py-3">data.transactions[index].date</td>
-    <td class="text-center py-3">data.transactions[index].amount</td>
-    <td class="text-center py-3">data.transactions[index].id</td>
-</tr>
-*/
